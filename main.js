7d78c8f654c7d71ae524514bbe8be2aa06438ad1
Narius17 <102422261+Narius17@users.noreply.github.com>
2022-03-28 22:41:33 +0300

    Выполнено четвертое задание (без усложнения)

---
+++ main.js
@@ -0,0 +1,26 @@
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelectorAll('.modal__button');
+const modalClose = document.querySelector('.modal_close');
+const body = document.querySelector('body');
+
+buttonModal.forEach((item)=>{
+    item.addEventListener ('click', ()=>{
+        modalWindow.style.display = 'flex';
+        body.classList.add('noscroll');
+    });
+});
+
+modalWindow.addEventListener('click', (e) => {
+    const isModal = e.target.closest ('.modal_inner');
+
+    if(!isModal) {
+        modalWindow.style.display = 'none';
+        body.classList.remove('noscroll');
+    }
+});
+
+modalClose.addEventListener('click', () =>{
+    modalWindow.style.display = 'none';
+        body.classList.remove('noscroll');
+})
+
